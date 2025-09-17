0b2a7737944a60de73c8a6358b0590e62ad58bb8
AlexeyShigenin <AlexeyShigenin@yandex.ru>
2025-09-17 23:56:50 +0300

    Добавлено задание 2_3_2

--- 2_3_BranchingLogicalOperators/2_3_BranchingLogicalOperators_1/2_3_BranchingLogicalOperators_1.cpp
+++ 2_3_BranchingLogicalOperators/2_3_BranchingLogicalOperators_1/2_3_BranchingLogicalOperators_1.cpp
@@ -1,5 +1,19 @@
 ﻿/*
+Задание 1. Таблица истинности
 Напишите программу, которая выводит таблицу истинности для логических операторов ||, &&, с применением логических выражений.
+
+Результат работы программы
+Оператор: ||
+true    true    true
+false   true    true
+true    false   true
+false   false   false
+
+Оператор: &&
+true    true    true
+false   true    false
+true    false   false
+false   false   false
 */
 
 #include <iostream>
@@ -8,25 +22,31 @@ int main()
 {
 	setlocale(LC_ALL, "rus");										// Настройка локали для поддержка вывода кирилицы в консоль
 	
+	bool a{1}, b{1};												// Объявление и инициализация переменных
+
 	std::cout << std::boolalpha;									// Ключ вывода bool в виде true/false, а не 1/0
 	
 	std::cout << "Оператор: ||" << std::endl;
-	bool a{}, b{};
-	std::cout << a << '\t' << b << '\t' << (a || b) << std::endl;	// false || false
-	b = true;
+	std::cout << a << '\t' << b << '\t' << (a || b) << std::endl;	// true || true
+	a = false;
 	std::cout << a << '\t' << b << '\t' << (a || b) << std::endl;	// false || true
 	a = true;
-	std::cout << a << '\t' << b << '\t' << (a || b) << std::endl;	// true || true
 	b = false;
 	std::cout << a << '\t' << b << '\t' << (a || b) << std::endl;	// true || false
+	a = false;
+	std::cout << a << '\t' << b << '\t' << (a || b) << std::endl;	// false || false
 		
 	std::cout << "\nОператор: &&" << std::endl;
-	a = false;
-	std::cout << a << '\t' << b << '\t' << (a && b) << std::endl;	// false && false
+	a = true;
 	b = true;
+	std::cout << a << '\t' << b << '\t' << (a && b) << std::endl;	// true && true
+	a = false;
 	std::cout << a << '\t' << b << '\t' << (a && b) << std::endl;	// false && true
 	a = true;
-	std::cout << a << '\t' << b << '\t' << (a && b) << std::endl;	// true && true
 	b = false;
 	std::cout << a << '\t' << b << '\t' << (a && b) << std::endl;	// true && false
+	a = false;
+	std::cout << a << '\t' << b << '\t' << (a && b) << std::endl;	// false && false
+
+	return EXIT_SUCCESS;
 }
